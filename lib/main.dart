19093088cdf1f31cf0961fde0ce4bf6005d6fd49
gurkangltekin <gurkangltekin@icloud.com>
2020-05-22 15:10:46 +0300

    Bazı eklemeler

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       new MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
@@ -48,9 +50,13 @@ void main() => runApp(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            FadeInImage.assetNetwork(placeholder: "assets/images/loading.gif", image: "https://gurkangltekin.com/wp-content/uploads/2020/04/kartvizitonyuz.png"),
+                            FlutterLogo(size: 60,
+                              colors: Colors.blue,
+                              style: FlutterLogoStyle.horizontal,
+                              textColor: Colors.black,),
+                            //FadeInImage.assetNetwork(placeholder: "assets/images/loading.gif", image: "https://gurkangltekin.com/wp-content/uploads/2020/04/kartvizitonyuz.png"),
                             Text(
-                              "Network Image",
+                              "Flutter Logo",
                             ),
                           ],
                         ),
@@ -63,7 +69,10 @@ void main() => runApp(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            Image.asset("assets/images/adsiz.jpg"),
+                            Expanded(
+                                child: Placeholder(color: Colors.red,
+                                  strokeWidth: 2.0,),
+                            ),
                             Text(
                               "Asset Image",
                             ),
@@ -106,7 +115,7 @@ void main() => runApp(
                           children: <Widget>[
                             CircleAvatar(
                               backgroundImage:
-                                  AssetImage("assets/images/adsiz.jpg"),
+                              AssetImage("assets/images/adsiz.jpg"),
                             ),
                             Text(
                               "Circle Asset Image",
@@ -133,6 +142,18 @@ void main() => runApp(
                         ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    RaisedButton(child: Text("Gürkan Gültekin"), onPressed: (){},),
+                    RaisedButton(child: Text("Flutter ve Dart Dersleri"), elevation: 12, textColor: Colors.yellow, color: Colors.blueGrey, onPressed: (){},),
+                    IconButton(icon: Icon(Icons.access_alarm), onPressed: (){}),
+                    FlatButton(onPressed: (){}, child: Text("Flat Button", style: TextStyle(fontSize: 15),),)
                   ],
                 ),
               ),
